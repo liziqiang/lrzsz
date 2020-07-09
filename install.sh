dcd35d6b556515740945f22b8a5dcaf7028ebcb8
Ziqiang Li <ziqiang.lee@gmail.com>
2020-07-09 18:16:41 +0800

    fix path

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ brew install lrzsz
 recv='/usr/local/bin/iterm2-recv-zmodem.sh'
 send='/usr/local/bin/iterm2-send-zmodem.sh'
 
-curl -sSL https://github.com/liziqiang/iterm2-zmodem/raw/master/iterm2-recv-zmodem.sh -o "$recv"
-curl -sSL https://github.com/liziqiang/iterm2-zmodem/raw/master/iterm2-send-zmodem.sh -o "$send"
+curl -sSL https://github.com/liziqiang/lrzsz/raw/master/iterm2-recv-zmodem.sh -o "$recv"
+curl -sSL https://github.com/liziqiang/lrzsz/raw/master/iterm2-send-zmodem.sh -o "$send"
 
 chmod +x "$recv" "$send"
 
