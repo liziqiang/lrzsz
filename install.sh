7a5b04f7cb638b1a44aab686dc2cce2b9dd473f4
Ziqiang Li <ziqiang.lee@gmail.com>
2020-07-09 16:00:55 +0800

    init

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+brew install lrzsz
+
+recv='/usr/local/bin/iterm2-recv-zmodem.sh'
+send='/usr/local/bin/iterm2-send-zmodem.sh'
+
+curl -sSL https://github.com/liziqiang/iterm2-zmodem/raw/master/iterm2-recv-zmodem.sh -o "$recv"
+curl -sSL https://github.com/liziqiang/iterm2-zmodem/raw/master/iterm2-send-zmodem.sh -o "$send"
+
+chmod +x "$recv" "$send"
+
+# set up iterm2 trigger
+# https://github.com/zzy0-0/iterm2-zmodem#setup
+
+# Set up Triggers in iTerm 2 like so:
+#     Regular expression: \*\*B0100
+#     Action: Run Silent Coprocess
+#     Parameters: /usr/local/bin/iterm2-send-zmodem.sh
+
+#     Regular expression: \*\*B00000000000000
+#     Action: Run Silent Coprocess
+#     Parameters: /usr/local/bin/iterm2-recv-zmodem.sh
+# To send a file to a remote machine:
+
+# Type "rz" on the remote machine
+# Select the file(s) on the local machine to send
+# Wait for the coprocess indicator to disappear
+# The receive a file from a remote machine
+
+# Type "sz filename1 filename2 … filenameN" on the remote machine
+# Select the folder to receive to on the local machine
+# Wait for the coprocess indicator to disappear
