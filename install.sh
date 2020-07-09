b1cd046b2118758857a0d29b811e87d663b48eec
Ziqiang Li <ziqiang.lee@gmail.com>
2020-07-09 19:26:32 +0800

    check if rz exist before install

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
-brew install lrzsz
+
+hash rz > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+    brew install lrzsz
+fi
 
 recv='/usr/local/bin/iterm2-recv-zmodem.sh'
 send='/usr/local/bin/iterm2-send-zmodem.sh'
